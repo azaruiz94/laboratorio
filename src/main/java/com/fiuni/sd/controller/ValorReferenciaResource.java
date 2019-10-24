30f887a15064b5b3e9df4554629a03850a81e37c
jodogocha <jodogocha@gmail.com>
2019-10-24 15:46:36 -0300

    Cambios en el controlador de valores de referencia

---
+++ src/main/java/com/fiuni/sd/controller/ValorReferenciaResource.java
@@ -0,0 +1,37 @@
+package com.fiuni.sd.controller;
+
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.fiuni.sd.dto.valor_referencia.ValorReferenciaDTO;
+import com.fiuni.sd.dto.valor_referencia.ValorReferenciaResult;
+import com.fiuni.sd.service.valor_referencia.IValorReferenciaService;
+import com.fiuni.sd.utils.Configuracion;
+
+@RestController
+@RequestMapping("/valoresReferencia")
+public class ValorReferenciaResource {
+	@Autowired
+	private IValorReferenciaService valorReferenciaService;
+
+	@GetMapping("/{id}")
+	public ValorReferenciaDTO getById(@PathVariable(value = "id") Integer valorReferenciaId) {
+		return valorReferenciaService.getById(valorReferenciaId);
+	}
+
+	@GetMapping(path = "/pag/{pag_num}")
+	public ValorReferenciaResult getValorReferencia(@PathVariable(value = "pag_num") Integer pagNum) {
+		return valorReferenciaService.getAll(PageRequest.of((pagNum - 1), Configuracion.PAGE_SIZE));
+	}
+
+	@PostMapping()
+	public ValorReferenciaDTO save(@Valid @RequestBody ValorReferenciaDTO valorReferencia) {
+		return valorReferenciaService.save(valorReferencia);
+	}
+}
