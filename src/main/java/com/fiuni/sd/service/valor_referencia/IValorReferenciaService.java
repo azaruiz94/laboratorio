76adef3e3c73eb8d32f5843b15e3896e5629f7a6
jodogocha <jodogocha@gmail.com>
2019-10-24 15:58:04 -0300

    Cambios en el service de valores de referencia

---
+++ src/main/java/com/fiuni/sd/service/valor_referencia/IValorReferenciaService.java
@@ -0,0 +1,9 @@
+package com.fiuni.sd.service.valor_referencia;
+
+import com.fiuni.sd.dto.valor_referencia.ValorReferenciaDTO;
+import com.fiuni.sd.dto.valor_referencia.ValorReferenciaResult;
+import com.fiuni.sd.service.base.IBaseService;
+
+public interface IValorReferenciaService extends IBaseService<ValorReferenciaDTO, ValorReferenciaResult> {
+
+}
