e25de2bf5c2d63999c6ac11715ae8850c0861bc2
jodogocha <jodogocha@gmail.com>
2019-10-25 13:25:54 -0300

    aplicamos log4j

--- src/main/java/com/fiuni/sd/LaboratorioApplication.java
+++ src/main/java/com/fiuni/sd/LaboratorioApplication.java
@@ -2,12 +2,26 @@ package com.fiuni.sd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.apache.log4j.Logger;
+import org.joda.time.LocalDate;
 
 @SpringBootApplication
 public class LaboratorioApplication {
+	
+	private static final Logger logger = Logger.getLogger(LaboratorioApplication.class);
 
 	public static void main(String[] args) {
+		System.out.println(getLocalCurrentDate());
 		SpringApplication.run(LaboratorioApplication.class, args);
 	}
+	
+	private static String getLocalCurrentDate() {
 
+		if (logger.isDebugEnabled()) {
+			logger.debug("getLocalCurrentDate() is executed!");
+		}
+
+		LocalDate date = new LocalDate();
+		return date.toString();
+	}
 }
