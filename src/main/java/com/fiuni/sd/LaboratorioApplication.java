ddea4fcfd0f5dcb122d2dd2b803742a295d9034e
william <willymarpuk@gmail.com>
2019-11-02 10:51:30 -0300

    Se modifico el archivo principal de la aplicacion para que se pueda usar cache y un archivo xml de configuracion

--- src/main/java/com/fiuni/sd/LaboratorioApplication.java
+++ src/main/java/com/fiuni/sd/LaboratorioApplication.java
@@ -2,10 +2,14 @@ package com.fiuni.sd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+//import org.springframework.context.annotation.ImportResource;
 import org.apache.log4j.Logger;
 import org.joda.time.LocalDate;
 
 @SpringBootApplication
+//@ImportResource("classpath:memcached.xml")
+@EnableCaching
 public class LaboratorioApplication {
 	
 	private static final Logger logger = Logger.getLogger(LaboratorioApplication.class);
